44670e44498ed27ea6b3a2fb5a3aeb557417cc34
Osorabrian <osorabrian@gmail.com>
2024-10-10 11:47:11 -0700

    create url patterns and url paths

---
+++ product/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from . import views
+
+app_name = "product"
+
+urlpattern= [
+    path('', views.products_list, name="products_list"),
+    path('<int:id>/', views.product_details, name="product_details")
+]

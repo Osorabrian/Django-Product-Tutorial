f6ae60b4daf8e844ca6162e8a899b72b0de02bd7
Osorabrian <osorabrian@gmail.com>
2024-10-10 09:43:48 -0700

    create retailer model and migrations

--- product/models.py
+++ product/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.utils import timezone
+from retailer.models import Retailer
 
 # Create your models here.
 class Product(models.Model):
@@ -23,9 +24,19 @@ class Product(models.Model):
     price = models.IntegerField()
     serial_number = models.CharField(max_length=250)
     stock_size = models.IntegerField()
+    
+    retailer = models.ForeignKey(
+        Retailer,
+        auto_delete = models.CASCADE,
+        related_name = 'products'
+    )
+    
     listed = models.DateTimeField(default=timezone.now)
     created = models.DateTimeField(auto_now_add=True)
     updated = models.DateTimeField(auto_now=True)
+
+    def __str__(self):
+        return self.name
     
     class Meta:
         ordering = ['-listed']
--- retailer/models.py
+++ retailer/models.py
@@ -1,3 +1,17 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+class Retailer(models.Model):
+    name = models.CharField(max_length=250)
+    slug = models.SlugField(max_length=250)
+    description = models.TextField()
+    registered = models.DateTimeField(default=timezone.now)
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
+    
+    def __str__(self):
+        return self.name
+    
+    class Meta:
+        indexes = [models.Index(fields=('name',))]

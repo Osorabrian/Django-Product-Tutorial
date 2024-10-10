984d514fc9b834769ee57fa00a1606a7d3ec2598
Osorabrian <osorabrian@gmail.com>
2024-10-10 10:34:58 -0700

    edit product admin

--- product/admin.py
+++ product/admin.py
@@ -5,6 +5,10 @@ from .models import Product
 @admin.register(Product)
 
 class ProductAdmin(admin.ModelAdmin):
-    list_display = []
-    list_filter = []
-    search_fields = []
+    list_display = ['name', 'serial_number', 'price', 'stock_size']
+    list_filter = ['name', 'listed', 'price']
+    search_fields = ['name', 'serial_number']
+    prepopulated_fields = {'slug': ('name',)}
+    date_hierarchy = 'listed'
+    raw_id_fields = ['retailer']
+    ordering = ['listed', 'name', 'price']

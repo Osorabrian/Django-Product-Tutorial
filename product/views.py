cecffb25afc90d0141771a393df9079b7443bc14
Osorabrian <osorabrian@gmail.com>
2024-10-17 05:35:06 -0700

    create product share form

--- product/views.py
+++ product/views.py
@@ -2,10 +2,10 @@ from django.shortcuts import get_object_or_404,render
 from .models import Product
 from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from django.views.generic import ListView
+from .forms import EmailPostForm
 
 class ProductListView(ListView):
-    # model = Product
-    queryset = Product.objects.all()
+    queryset = Product.objects.all() # or model = Product
     context_object_name = 'products'
     paginate_by = 3
     template_name = 'product/list.html'
@@ -20,8 +20,7 @@ class ProductListView(ListView):
 #     except EmptyPage:
 #         products = paginator.page(paginator.num_pages)
 #     except PageNotAnInteger:
-#         products = paginator.page(1)
-        
+#         products = paginator.page(1)     
 #     return render(request, 'product/list.html', {'products': products})
 
 def product_details(request, year, month, day, post):
@@ -33,3 +32,21 @@ def product_details(request, year, month, day, post):
         listed__day = day
     )
     return render(request, 'product/detail.html', {'product': product})
+
+def post_form(request, id):
+    post = get_object_or_404(
+        Product,
+        id=id
+    )
+    
+    if request.method == 'POST':
+        form = EmailPostForm(request.POST)
+        if form.is_valid():
+            cd = form.cleaned_data
+    else:
+        form = EmailPostForm()
+    
+    return render(
+        request,
+        'product/share.html'
+    )

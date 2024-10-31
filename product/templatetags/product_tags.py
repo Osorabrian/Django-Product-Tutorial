d21c0deeacafbaf57540d769e8d71f1204a60b36
Osorabrian <osorabrian@gmail.com>
2024-10-31 15:28:04 -0700

    add markdown custom filter

--- product/templatetags/product_tags.py
+++ product/templatetags/product_tags.py
@@ -1,6 +1,8 @@
 from django import template
 from ..models import Product
 from django.db.models import Count
+from django.utils.safestring import mark_safe
+import markdown
 
 register = template.Library()
 
@@ -16,3 +18,7 @@ def most_recent_listings(count = 5):
 @register.simple_tag
 def most_commented_listings():
     return Product.objects.annotate( total_comments = Count('comments')).order_by('total_comments')[:3]
+
+@register.filter(name="markdown")
+def markdown_format(text):
+    return mark_safe(markdown.markdown(text))

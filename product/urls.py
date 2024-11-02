738085691478410375022c89a1efbdc51927c5f7
Osorabrian <osorabrian@gmail.com>
2024-11-01 19:33:44 -0700

    addd RSS feed in the website

--- product/urls.py
+++ product/urls.py
@@ -10,5 +10,5 @@ urlpatterns = [
     path('<int:year>/<int:month>/<int:day>/<slug:post>/', views.product_details, name="product_details"),
     path('<int:id>/share/', views.product_share, name="product_share"),
     path('<int:id>/comment/', views.product_comment, name='product_comment'),
-    path('feed/', LatestListings(), name='product_listing')
+    path('feed/', LatestListings(), name='product_feed')
 ]
